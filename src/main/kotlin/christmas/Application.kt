15e00bde7f7ef3d4af449cf159bb5c555ecfbf28
eunsxx <88652027+eunsxx@users.noreply.github.com>
2023-11-15 18:18:46 +0900

    [Fix] #12 exchange readLine() to Console.readLine()

--- src/main/kotlin/christmas/Application.kt
+++ src/main/kotlin/christmas/Application.kt
@@ -1,5 +1,6 @@
 package christmas
 
+import camp.nextstep.edu.missionutils.Console
 import java.time.DayOfWeek
 import java.time.LocalDate
 
@@ -23,29 +24,39 @@ const val START_DISCOUNT_PRICE = 1_000
 const val DISCOUNT_PRICE = 100
 const val WEEK_DISCOUNT_PRICE = 2_023
 
+val menuMapping = mapOf(
+    "양송이수프" to "MUSHROOM_SOUP",
+    "타파스" to "TAPAS",
+    "시저샐러드" to "CAESAR_SALAD",
+    "티본스테이크" to "T_BONE_STEAK",
+    "바비큐립" to "BBQ_RIBS",
+    "해산물파스타" to "SEAFOOD_PASTA",
+    "크리스마스파스타" to "CHRISTMAS_PASTA",
+    "초코케이크" to "CHOCOLATE_CAKE",
+    "아이스크림" to "ICE_CREAM",
+    "제로콜라" to "ZERO_COLA",
+    "레드와인" to "RED_WINE",
+    "샴페인" to "CHAMPAGNE"
+)
 fun main() {
-    try {
-        val input = InputView()
-        val output = OutputView()
+    val input = InputView()
+    val output = OutputView()
 
-        val date = input.readDate()
-        val order = input.readMenu()
+    val date = input.readDate()
+    val order = input.readMenu()
 
-        val mappingOrder = order.mapKeys { (key, _) -> translateToEnglishName(key) }
+    val mappingOrder = order.mapKeys { (key, _) -> translateToEnglishName(key) }
 
-        println("12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!")
-        output.printMenu(order)
-        val totalPrice = calculateTotalPrice(mappingOrder)
-        output.printTotalPrice(totalPrice)
-        output.printGift(totalPrice)
-        output.printBenefit(date, totalPrice, mappingOrder)
-        val totalDiscount = discountedTotal(date, totalPrice, mappingOrder)
-        output.printTotalDiscount(totalDiscount)
-        output.estimatedPaymentAmount(totalPrice, totalDiscount)
-        output.printEventBedge(totalDiscount)
-    } catch (e: IllegalArgumentException) {
-        println(e.message)
-    }
+    println("12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!")
+    output.printMenu(order)
+    val totalPrice = calculateTotalPrice(mappingOrder)
+    output.printTotalPrice(totalPrice)
+    output.printGift(totalPrice)
+    output.printBenefit(date, totalPrice, mappingOrder)
+    val totalDiscount = discountedTotal(date, totalPrice, mappingOrder)
+    output.printTotalDiscount(totalDiscount)
+    output.estimatedPaymentAmount(totalPrice, totalDiscount)
+    output.printEventBedge(totalDiscount)
 }
 fun checkWeekend(year: Int, month: Int, day: Int): Boolean {
     val date = LocalDate.of(year, month, day)
@@ -75,20 +86,31 @@ fun christmasDDayDiscount(date: Int): Int {
 
 class InputView {
     fun readDate(): Int {
-        println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)")
-        val date = readLine()?.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.")
-        require(date in 1..31) { "[ERROR] 1부터 31 사이의 숫자를 입력해 주세요." }
-        return date
+        return try {
+            println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)")
+            val input = Console.readLine()
+            val date = input.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.")
+            require(date in 1..31) { "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요." }
+            date
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            readDate()
+        }
     }
 
     fun readMenu(): Map<String, Int> {
-        println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
-        val input = readLine() ?: throw IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
-        val order = parseOrder(input)
-        val mappingOrder = order.mapKeys { (key, _) -> translateToEnglishName(key) }
-        checkMenuRules(mappingOrder)
+        return try {
+            println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
+            val input = Console.readLine() ?: throw IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+            val order = parseOrder(input)
+            val mappingOrder = order.mapKeys { (key, _) -> translateToEnglishName(key) }
+            checkMenuRules(mappingOrder)
 
-        return order
+            order
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            readMenu()
+        }
     }
 }
 fun calculateTotalPrice(order: Map<String, Int>): Int {
@@ -106,20 +128,7 @@ fun calculateTotalPrice(order: Map<String, Int>): Int {
 
     return totalPrice
 }
-val menuMapping = mapOf(
-    "양송이수프" to "MUSHROOM_SOUP",
-    "타파스" to "TAPAS",
-    "시저샐러드" to "CAESAR_SALAD",
-    "티본스테이크" to "T_BONE_STEAK",
-    "바비큐립" to "BBQ_RIBS",
-    "해산물파스타" to "SEAFOOD_PASTA",
-    "크리스마스파스타" to "CHRISTMAS_PASTA",
-    "초코케이크" to "CHOCOLATE_CAKE",
-    "아이스크림" to "ICE_CREAM",
-    "제로콜라" to "ZERO_COLA",
-    "레드와인" to "RED_WINE",
-    "샴페인" to "CHAMPAGNE"
-)
+
 fun translateToEnglishName(menuName: String): String {
     return menuMapping[menuName] ?: throw IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
 }
